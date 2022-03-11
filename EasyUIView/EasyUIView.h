865829616e41335d2bfb36662ff73c75ac919c39
shrishti <shrishtisingh323@gmail.com>
2022-03-11 12:37:50 +0530

    Initial project setup

---
+++ EasyUIView/EasyUIView.h
@@ -0,0 +1,18 @@
+//
+//  EasyUIView.h
+//  EasyUIView
+//
+//  Created by shrishti on 11/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyUIView.
+FOUNDATION_EXPORT double EasyUIViewVersionNumber;
+
+//! Project version string for EasyUIView.
+FOUNDATION_EXPORT const unsigned char EasyUIViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyUIView/PublicHeader.h>
+
+
