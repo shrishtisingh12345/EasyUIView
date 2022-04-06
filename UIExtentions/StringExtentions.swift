50991433ec545bc0f168f748b8f78db0e29b1b72
shrishti <shrishtisingh323@gmail.com>
2022-04-06 15:52:37 +0530

    Commented some code

--- UIExtentions/StringExtentions.swift
+++ UIExtentions/StringExtentions.swift
@@ -9,17 +9,17 @@ import Foundation
 import UIKit
 
 public extension UILabel {
-    
-    func attributeString(text1:String,text2:String) -> NSAttributedString {
-      // let underLineStyle = NSUnderlineStyle.single.rawValue
-        let attrs2 = [
-                NSAttributedString.Key.foregroundColor: UIColor(red: 0.875, green: 0.271, blue: 0.251, alpha:1),
-                NSAttributedString.Key.underlineColor: UIColor(red: 0.875, green: 0.271, blue: 0.251, alpha:1),
-               // NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
-        ] as [NSAttributedString.Key : Any]
-
-      let strings1 = NSMutableAttributedString(string: text1, attributes: attrs2)
-        return strings1
-    }
+//    
+//    func attributeString(text1:String,text2:String) -> NSAttributedString {
+//      // let underLineStyle = NSUnderlineStyle.single.rawValue
+//        let attrs2 = [
+//                NSAttributedString.Key.foregroundColor: UIColor(red: 0.875, green: 0.271, blue: 0.251, alpha:1),
+//                NSAttributedString.Key.underlineColor: UIColor(red: 0.875, green: 0.271, blue: 0.251, alpha:1),
+//               // NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
+//        ] as [NSAttributedString.Key : Any]
+//
+//      let strings1 = NSMutableAttributedString(string: text1, attributes: attrs2)
+//        return strings1
+//    }
     
 }
